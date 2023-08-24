8b1f08a7eeac58dc8b277623dbebe7fe2137e8da
Laura <Laura@DESKTOP-BESSB6E>
2023-08-25 00:44:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Iveskite 3 skaicius: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number == number2 && number3 == number)
+{
+    Console.WriteLine("Visi skaiciai yra lygus");
+}
+else if (number == number2 || number == number3 || number2 == number3 )
+{
+    Console.WriteLine("Du skaiciai yra lygus");
+}
+else
+{
+    Console.WriteLine("Ne vienas skaicius nera lygus");
+}
